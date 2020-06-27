f7b7f60427ba3606ed6c05e787acf9e8bc92e8c4
Srikanth Kothala <kothala.srikanth@gmail.com>
2020-06-28 01:37:36 +0530

    mail

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "kothala.srikant@gmail.com";
+        $recipient = "kothala.srikanth@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
